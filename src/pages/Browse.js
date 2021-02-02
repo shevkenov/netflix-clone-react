a7dcd4d815c911d5f1d46135d8606c3d237f70fa
shevkenov <shevkenov@gmail.com>
2021-02-02 10:15:33 +0200

    add card component to Browse page

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import useContent from "../hooks/use-content";
 import { BrowseContainer } from '../containers/browse';
+import selectionMap from '../utils/selection-map';
 
 export default function Browse() {
-    return <BrowseContainer />
+    
+    const {series} = useContent("series");
+    const {films} = useContent("films");
+    const slides = selectionMap({films, series});
+
+    return <BrowseContainer content={slides}/>
 }
